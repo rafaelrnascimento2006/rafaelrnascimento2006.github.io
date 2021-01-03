9a451c8c8e4597108020f3f6b69aa91fa94951fb
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-01-03 16:11:05 -0300

    estilizando portfolio

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,10 +1,8 @@
 function randomColor() {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  const rgbArray = [red, green, blue];
-  const color = `rgb(${rgbArray})`;
-  return color;
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 window.onload = function () {
